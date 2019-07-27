eb0992fcddf6f576ce6a8e6913e25ad181c4164a
Bhuvan Belur <bhuvan.belur@westminster.org.uk>
2019-07-28 00:24:18 +0530

    Fixed loading while loggin in or registering

--- Assets/Scripts/Account/LoginManager.cs
+++ Assets/Scripts/Account/LoginManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.Networking;
 using System.Security.Cryptography;
 using System.Text;
 using UnityEngine.SceneManagement;
-using AssemblyCSharp.Assets.Scripts.Medo.Security;
+using System.Threading;
 using AssemblyCSharp.Assets.Scripts.Medo.Security.Cryptography;
 
 public class LoginManager : MonoBehaviour
@@ -17,57 +17,94 @@ public class LoginManager : MonoBehaviour
     public GameObject characNameInput;
 
     public GameObject errorDisplay;
+    public GameObject loadingIndicator;
 
     public GameObject tosAccepted;
 
+    bool logginIn;
+    bool registering;
+
+    string username;
+    string password;
+    string characterName;
+    string value;
+
     Dictionary<string, string> errors = new Dictionary<string, string>() { { "0", "Invalid credentials" }, { "1", "You need to verify your account! Check email." }, { "2", "An account with this email adress already exists."} };
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        loadingIndicator.SetActive(false);
     }
 
-    // Update is called once per frame
+    // this is all done in states now, so that 
     void Update()
     {
-        
+        if (logginIn)
+        {
+            if (value != null)
+            {
+                logginIn = false;
+                StartCoroutine(loginCoroutine(username, value));
+            }
+        }
+        else if (registering)
+        {
+            if (value != null)
+            {
+                registering = false;
+                StartCoroutine(registerCoroutine(username, value, characterName));
+            }
+        }
     }
 
     public void login()
     {
-        string username = usernameInput.GetComponent<InputField>().text;
-        string password = passwordInput.GetComponent<InputField>().text;
-        string hash = Encoding.UTF8.GetString(PBKDF2(password, Encoding.UTF8.GetBytes("thisisepic"), 64000, 18));
-        
-        StartCoroutine(loginCoroutine(username, hash));
+        username = usernameInput.GetComponent<InputField>().text;
+        password = passwordInput.GetComponent<InputField>().text;
+
+        value = null;
+        var thread = new Thread(
+            () =>
+            {
+                value = Encoding.UTF8.GetString(PBKDF2(password, Encoding.UTF8.GetBytes("thisisepic"), 64000, 18));
+            });
+        thread.Start();
+        logginIn = true;
     }
 
     public void register()
     {
-        string username = usernameInput.GetComponent<InputField>().text;
-        string password = passwordInput.GetComponent<InputField>().text;
-        string characterName = characNameInput.GetComponent<InputField>().text;
-        string hash = Encoding.UTF8.GetString(PBKDF2(password, Encoding.UTF8.GetBytes("thisisepic"), 64000, 18));
+        username = usernameInput.GetComponent<InputField>().text;
+        password = passwordInput.GetComponent<InputField>().text;
         if (username.Contains("@"))
         {
             if (password.Length >= 6)
             {
                 if (tosAccepted.GetComponent<Toggle>().isOn)
                 {
-                    StartCoroutine(registerCoroutine(username, hash, characterName));
+                    errorDisplay.GetComponent<Text>().text = "";
+                    loadingIndicator.SetActive(true);
+                    registering = true;
+                    username = usernameInput.GetComponent<InputField>().text;
+                    password = passwordInput.GetComponent<InputField>().text;
+                    characterName = characNameInput.GetComponent<InputField>().text;
+                    value = null;
+                    var thread = new Thread(
+                        () =>
+                        {
+                            value = Encoding.UTF8.GetString(PBKDF2(password, Encoding.UTF8.GetBytes("thisisepic"), 64000, 18));
+                        });
+                    thread.Start();
                 }
                 else
                 {
                     errorDisplay.GetComponent<Text>().text = "You must accept the terms of service.";
                 }
-                
             }
             else
             {
                 errorDisplay.GetComponent<Text>().text = "Your password must be 6 characters or more!";
             }
-            
         }
         else
         {
@@ -88,23 +125,29 @@ public class LoginManager : MonoBehaviour
 
     IEnumerator registerCoroutine(string username, string password, string characterName)
     {
-
         WWWForm form = new WWWForm();
         form.AddField("username", username);
         form.AddField("password", password);
         form.AddField("characterName", characterName);
         UnityWebRequest request = UnityWebRequest.Post("https://arx-noctis-backend.herokuapp.com/register", form);
         yield return request.Send();
-        Debug.Log(request.downloadHandler.text);
         LoginResp resp = JsonUtility.FromJson<LoginResp>(request.downloadHandler.text);
+        loadingIndicator.SetActive(false);
         if (resp.resp)
         {
-            errorDisplay.GetComponent<Text>().text = "Great! Check your email to verify your account!";
+            errorDisplay.GetComponent<Text>().text = "Thanks for signing up! Please verify your email.";
+            StartCoroutine(waitToLogin(3.0f));
         }
         else
         {
             errorDisplay.GetComponent<Text>().text = errors[resp.failCode];
         }
+    }
+
+    IEnumerator waitToLogin(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Login");
     }
 
     IEnumerator loginCoroutine(string username, string password)
@@ -114,8 +157,8 @@ public class LoginManager : MonoBehaviour
         form.AddField("password", password);
         UnityWebRequest request = UnityWebRequest.Post("https://arx-noctis-backend.herokuapp.com/authenticate", form);
         yield return request.Send();
-        Debug.Log(request.downloadHandler.text);
         LoginResp resp = JsonUtility.FromJson<LoginResp>(request.downloadHandler.text);
+        loadingIndicator.SetActive(false);
         if (resp.resp)
         {
             PlayerPrefs.SetString("playerName", resp.characterName);
@@ -126,6 +169,7 @@ public class LoginManager : MonoBehaviour
         else
         {
             errorDisplay.GetComponent<Text>().text = errors[resp.failCode];
+            yield break;
         }
 
     }
---
+++ Assets/Scripts/Helpers/LoadingText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingText : MonoBehaviour
+{
+
+    Text text;
+    int count = 0;
+    int stage = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (count >= 30)
+        {
+            stage++;
+            if (stage < 4)
+            {
+                text.text = text.text + " .";
+            }
+            else if (stage == 4)
+            {
+                stage = 0;
+                text.text = "Loading";
+            }
+
+            count = 0;
+        }
+        count++;
+    }
+}
